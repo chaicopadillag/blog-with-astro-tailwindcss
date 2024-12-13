c329e8650f34f55630c18c78e768c1d21a7420ce
chaicopadillag <xt9738@rimac.com.pe>
2024-12-13 13:54:03 -0500

    Add author profiles and update blog post authors to reference new author data structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,35 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 
 const blog = defineCollection({
   type: 'content',
   schema: ({ image }) =>
     z.object({
       title: z.string(),
-      author: z.string(),
+      author: reference('author'),
       date: z.date(),
       image: image().refine((img) => img.width < 1200, {
         message: 'Image should be lower than 1200px'
       }),
       tags: z.array(z.string()),
-      description: z.string()
+      description: z.string(),
+      isDraft: z.boolean().default(false)
     })
 });
 
-export const collections = { blog };
+const author = defineCollection({
+  type: 'data',
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image().refine((img) => img.width < 1200, {
+        message: 'Image should be lower than 1200px'
+      }),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string()
+    })
+});
+
+export const collections = { blog, author };
