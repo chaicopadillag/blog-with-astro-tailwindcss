2c7d41e760a799cc49233537bf763d4050de92b2
chaicopadillag <xt9738@rimac.com.pe>
2024-12-13 17:37:44 -0500

    Rename blog post file extension from .mdx to .md and update title; add Markdown and HTML sanitization for RSS feed content

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request, site }) => {
   const blog = await getCollection('blog');
@@ -9,11 +13,25 @@ export const GET: APIRoute = async ({ params, request, site }) => {
     title: 'Blog with Astro',
     description: 'A blog built with Astro',
     site: site!,
-    items: blog.map(({ data, slug }) => ({
+    items: blog.map(({ data, slug, body }) => ({
       title: data.title,
       description: data.description,
       link: `/blog/${slug}`,
-      pubDate: data.date
-    }))
+      pubDate: data.date,
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+
+      customData: `<media:content
+    type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+    width="${data.image.width}"
+    height="${data.image.height}"
+    medium="image"
+    url="${site + data.image.src}" />
+`
+    })),
+    xmlns: {
+      media: 'http://search.yahoo.com/mrss/'
+    }
   });
 };
